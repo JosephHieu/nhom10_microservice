e17cd86da0cc2e99638e0d9acaa18f484d373d62
josephhieu <joseph.nguyenhoanghieu@gmail.com>
2025-09-16 21:55:15 +0700

    Tích hợp Eureka Discovery Service

---
+++ springboot-project/discovery-server/src/test/java/com/josephhieu/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josephhieu.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
